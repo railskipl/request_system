092dada0bae80819877fbf450fc2dbd60377df56
KIPL <kipl@kipl-Inspiron-620s.(none)>
2012-12-17 18:36:53 +0530

    changes

--- app/assets/javascripts/nested_form.js
+++ app/assets/javascripts/nested_form.js
@@ -7,7 +7,7 @@ function add_fields(link, association, content) {
    if(jQuery(".fields :file").length < 5) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  $(link).up().insert({
+  jQuery(link).up().insert({
     before: content.replace(regexp, new_id)
   });
 }
