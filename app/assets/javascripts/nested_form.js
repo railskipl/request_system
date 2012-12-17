6c3aaf04915b414dc38d30db4dd90b9cd0046ac5
KIPL <kipl@kipl-Inspiron-620s.(none)>
2012-12-17 18:47:43 +0530

    changes

--- app/assets/javascripts/nested_form.js
+++ app/assets/javascripts/nested_form.js
@@ -7,7 +7,7 @@ function add_fields(link, association, content) {
    if(jQuery(".fields :file").length < 5) {
   var new_id = new Date().getTime();
   var regexp = new RegExp("new_" + association, "g")
-  jQuery(link).up().insert({
+  $(link).up().insert({
     before: content.replace(regexp, new_id)
   });
 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 MiniApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # In the development environment your application's code is reloaded on
+  # In the production environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
@@ -14,11 +14,11 @@ MiniApp::Application.configure do
   config.action_controller.perform_caching = false
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'example.com' }
   config.action_mailer.delivery_method = :smtp
   # change to true to allow email to be sent during development
-  config.action_mailer.perform_deliveries = false
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
